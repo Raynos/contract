bb584f03ec1bdd28071968f54a514e3e45a60058
Raynos <raynos2@gmail.com>
2011-08-31 21:24:17 +0100

    added index.html and updated files

--- src/contract.js
+++ src/contract.js
@@ -1,36 +1,70 @@
+// The contract object.
 var Contract = {
-	"pre": function _pre(pre, f) {
-		var g = function _wrapped() {
-			pre.apply(this, arguments);
-			return f.apply(this, arguments);
-		};
-
-		if (this._wrapped) {
-			f = this._wrapped;
-			this._wrapped = g;
-			return this;
-		} else if (arguments.length === 1) {
-			f = this;
-			return g;
-		} else {
-			return g;
-		}
-	},
-	"post": function _post() {
-		
-	},
+	// A method that generates a function which will wrap the function and
+	// call the pre condition with (arg1, arg2, ...)
+    "_constructPre": function _constructPre(f, pre) {
+        return function _wrapped() {
+            pre.apply(this, arguments);
+            return f.apply(this, arguments);
+        };
+    },
+    // handle multiple APIs. Pass the function & pre conditions into constructPre
+    "pre": function _pre(pre, f) {
+        return this._handleMultipleAPIs(f, pre, "_constructPre");
+    },
+    // A method that generates a function which will wrap the function and 
+    // call the post condition with (ret, arg1, arg2, ...)
+    "_constructPost": function _constructPost(f, post) {
+        return function _wrapped() {
+            var arr = [f.apply(this, arguments)];
+            post.apply(this, arr.concat.apply(arr, arguments));
+            return arr[0];
+        }
+    },
+    // Handle multiple APIs. Either this was wrapped `construct(f).pre(pre)` or
+    // This was called from the function prototype `f.pre(pre)` or this was 
+    // called from the Construct object `Construct.pre(pre, f)` 
+    "_handleMultipleAPIs": function _handleMultipleAPIs(f, obj, method) {
+        if (this._wrapped) {
+            f = this._wrapped;
+            this._wrapped = this[method](f, obj);
+            return this;
+        } else if (arguments.length === 1) {
+            f = this;
+            return this[method](f, obj);
+        } else {
+            return this[method](f, obj);
+        }
+    },
+    // handle multiple APIs. Pass the function & post condition into constructPost
+    "post": function _post(post, f) {
+        return this._handleMultipleAPIs(f, post, "_constructPost");
+    },
+    // un-implemented TODO
 	"invariant": function _invariant() {
 		
 	},
+	// unwrap the Constructo object
 	"valueOf": function _valueOf() {
 		return this._wrapped;
 	}
 };
 
+// defaults object. By default inject into Function.prototype
 var	defaults = {
 	natives: true
 };
 
+// ContractFactory generates a new Contract based on the passed function `f`
+var ContractFactory = function _create(f) {
+	var c = Object.create(Contract);
+	c._wrapped = f;
+	return c;
+}
+
+// contract = require("constract");
+// Sets up the contract object basedon the options.
+// If `natives === true` inject the methods into Function.prototype.
 module.exports = function(options) {
 	Object.keys(defaults).forEach(function(key) {
 		if (options && options[key]) {
@@ -42,15 +76,13 @@ module.exports = function(options) {
 		defineProperties(Function.prototype)
 	}
 
-	return function _create(f) {
-		var c = Object.create(Contract);
-		c._wrapped = f;
-		return c;
-	};
+	return ContractFactory;
 };
 
-defineProperties(module.exports);
+// define the properties on `ContractFactory`
+defineProperties(ContractFactory);
 
+// Define the pre, post & invariant objects onto an object.
 function defineProperties(obj) {
 	Object.defineProperties(obj, {
 		"pre": {
--- test/contract-test.js
+++ test/contract-test.js
@@ -49,6 +49,7 @@ var suite = is.suite("contract").batch()
 
 	.context("the function prototype")
 		.topic.is(function() { return Function.prototype; })
+		.vow.it.should.be.a("function")
 		.partial("contains pre")
 		.partial("contains post")
 		.partial("contains invariant")
@@ -57,11 +58,10 @@ var suite = is.suite("contract").batch()
 
 	.context("contract(f).pre(g) ")
 		.topic.is(function() {
-			console.log("called");
 			var promise = ev();
 
 			var c = contract(function(a, b) {
-				
+				return a;
 			});
 			c = c.pre(function(a,b) {
 				_should.exist(a);
@@ -73,12 +73,63 @@ var suite = is.suite("contract").batch()
 
 			return c.valueOf();
 		})
+		.vow.it.should.be.a("function")
 		.context("when called (1)")
 			.topic.is.an.invocation(1)
 			.vow.it.should.error
+			.vow.it.should.have
+				.property("message", "expected undefined to exist")
+			.parent()
+		.context("when called (1,2)")
+			.topic.is.an.invocation(1,2)
+			.vow.it.should.error
+			.vow.it.should.have
+				.property("message", "expected 2 to be a string")
+			.parent()
+		.context("when called (1, 'foo')")
+			.topic.is.an.invocation(1, 'foo')
+			.vow.it.should.error
+			.vow.it.should.have
+				.property("message", "expected 1 to be above 5")
+			.parent()
+		.context("when called (6, 'foo')")
+			.topic.is.an.invocation(6, 'foo')
+			.vow.it.should.not.error
+			.vow.it.should.equal(6)
 			.suite()
-				
 
+.batch()
+	
+	.context("contract(f).post(g) ")
+		.topic.is(function() {
+			var promise = ev();
+
+			var c = contract(function(a, b) {
+				return a;
+			})
+			c = c.post(function(ret, a, b) {
+				console.log(arguments);
+				ret.should.equal(b)
+			});
+
+			return c.valueOf();
+		})
+		.vow.it.should.be.a("function")
+		.context("when called (5, 6)")
+			.topic.is.an.invocation(5, 6)
+			.vow.it.should.error
+			.vow.it.should.have
+				.property("message", "expected 5 to equal 6")
+			.parent()
+		.context("when called (5, 5)")
+			.topic.is.an.invocation(5, 5)
+			.vow.it.should.not.error
+			.vow.it.should.equal(5)
+			.parent()
+			
+
+				
+.suite();
 
 	
 if (module.parent) {
